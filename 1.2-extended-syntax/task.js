784e7775b3572ab53039e32d6888723e36959d8b
Violetta Mikhaylovskaya <59100268+Vitaas@users.noreply.github.com>
2020-04-12 18:15:49 +0300

    ДЗ 2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,61 @@
+'use strict';
+
+let a = 2;
+let b = 4;
+let c = -3;
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+  let x = (-b / 2 * a);
+  let x1 = (-4 + Math.sqrt(d)) / 2 * 2;
+  let x2 = (-4 - Math.sqrt(d)) / 2 * 2;
+  let result = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return {d: d, result: result};
+  } else if (d === 0) {
+    result.push(x);
+    return {d: d, result: result};
+  } else {
+    result.push(x1);
+    result.push(x2);
+    return {d: d, result: result};
+  }
 }
+
+let marks = [4, 5, 5, 3, 4, 3, 5];
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sumMarks = 0;
+  let averageMark = 0;
+  if (marks.lenght == 0) {
+    averageMark = 0;
+  } else if (marks.lenght <= 5) {
+    for (let i = 0; 1 <= marks.lenght; i++) {
+      sum += marks[i];
+      averageMark = sumMarks / marks.lenght;
+    }
+  } else {
+    console.log('Оценок больше 5, средняя будет считаться по первым пяти оценкам');
+    let marksFive = marks.splice(0, 5);
+    for (let i = 0; 1 <= marksFive.lenght; i++) {
+      sum += marksFive[i];
+      averageMark = sumMarks / marksFive.lenght;
+    }
+  }
+  return averageMark;
 }
 
+name = "Иван";
+dateOfBirthday = 20;
+
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let now = new Date();
+  let age = now.getFullYear() - dateOfBirthday.getFullYear();
+  
+  if (age > 18) {
+    return console.log(`Не желаете ли олд-фэшн, ${name}?`);
+  } else {
+    return console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+  }  
 }
