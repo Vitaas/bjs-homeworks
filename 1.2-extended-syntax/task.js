867726fab60036c8c0bbd0038851a5f910879341
Violetta Mikhaylovskaya <59100268+Vitaas@users.noreply.github.com>
2020-04-13 23:09:33 +0300

    corrections were done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,61 +1,54 @@
 'use strict';
 
-let a = 2;
-let b = 4;
-let c = -3;
-
 function getResult(a,b,c){
-  let x = (-b / 2 * a);
-  let x1 = (-4 + Math.sqrt(d)) / 2 * 2;
-  let x2 = (-4 - Math.sqrt(d)) / 2 * 2;
-  let result = [];
+  
   let d = Math.pow(b, 2) - 4 * a * c;
+  let x = (-b / 2 * a);
+  let x1 = (-b + Math.sqrt(d)) / 2 * a;
+  let x2 = (-b - Math.sqrt(d)) / 2 * a;
+  let result = [];
+  
 
   if (d < 0) {
-    return {d: d, result: result};
+    return result.push[''];
   } else if (d === 0) {
-    result.push(x);
-    return {d: d, result: result};
+    return result.push(x);
   } else {
-    result.push(x1);
-    result.push(x2);
-    return {d: d, result: result};
+    return result.push(x1, x2);
   }
 }
 
 let marks = [4, 5, 5, 3, 4, 3, 5];
 
 function getAverageMark(marks){
-    let sumMarks = 0;
+
+  let sumMarks = 0;
   let averageMark = 0;
-  if (marks.lenght == 0) {
-    averageMark = 0;
-  } else if (marks.lenght <= 5) {
-    for (let i = 0; 1 <= marks.lenght; i++) {
-      sum += marks[i];
-      averageMark = sumMarks / marks.lenght;
-    }
-  } else {
+
+  if (marks.length > 5) {
     console.log('Оценок больше 5, средняя будет считаться по первым пяти оценкам');
-    let marksFive = marks.splice(0, 5);
-    for (let i = 0; 1 <= marksFive.lenght; i++) {
-      sum += marksFive[i];
-      averageMark = sumMarks / marksFive.lenght;
+    let marks = marks.splice(0, 5);
+  } else if (marks.lenght == 0) {
+    return 0;
+  } else {
+    for (let i = 0; 5 >= marks.length; i++) {
+      sum += marks[i];
+      averageMark = sumMarks / marks.length;
     }
-  }
-  return averageMark;
+    return averageMark;
 }
 
-name = "Иван";
-dateOfBirthday = 20;
+getAverageMark(marks);
 
-function askDrink(name,dateOfBirthday){
-    let now = new Date();
+function askDrink(name, dateOfBirthday) {
+  let now = new Date();
   let age = now.getFullYear() - dateOfBirthday.getFullYear();
   
   if (age > 18) {
-    return console.log(`Не желаете ли олд-фэшн, ${name}?`);
+    return `Не желаете ли олд-фэшн, ${name}?`;
   } else {
-    return console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
-  }  
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+  }
 }
+
+askDrink('Иван', 20);
