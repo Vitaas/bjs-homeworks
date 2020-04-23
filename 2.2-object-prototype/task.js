91640621f5bbc38b5aab9402c8b0872503d88cd7
Violetta Mikhaylovskaya <59100268+Vitaas@users.noreply.github.com>
2020-04-24 00:35:22 +0300

    Version 1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,41 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
-}
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    if (animal === undefined) {
+      return null;
+  
+    } else {
+      let sound = animal.sound;
+      return sound;
+    }
+  }
 
+function getAverageMark(marks){
+  let sumMarks = 0;
+    
+  if (marks.lenght == 0) {
+    return 0;
+  }
+    
+  for(let i = 0; i < marks.length; i++) {
+    sumMarks += marks[i];
+  }
+    
+  const averageMark = sumMarks / marks.length;
+  const roundedAverage = Math.round(averageMark);
+    
+  return roundedAverage;
+}
+    
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+  const now = Date.now();
+  const birth = +new Date(1998, 2, 26);
+  const diff = now - birth;
+  const age = diff / (365.25 * 24 * 60 * 60 * 1000);
+        
+  if(age >= 18) {
+    return verdict = console.log('Совершеннолетний');
+  } else {
+    return verdict = console.log('Несовершеннолетний');
+  }
 }
